949286ad62597b77dfd7b8ff05c9d8d401d15aca
licheng173 <cl3570@nyu.edu>
2016-12-10 11:10:37 -0500

    final_graph
    全部搞定 输入nodelist， 调用createGraph（）checkAtomicity（） 验证atomic性质

--- Project2/Node.java
+++ Project2/Node.java
@@ -1,19 +1,24 @@
-import java.util.*;
-
+import  java.util.*;
 public class Node {
     public int start;
     public int end;
     public int value;
     public int type;
-    public ArrayList<Node> next;
-    public ArrayList<Node> nextHybrid;
+    public HashSet<Node> next;
+    public HashSet<Node> nextHybrid;
 
-    public Node(int s, int e, int v, int t, ArrayList<Node> next, ArrayList<Node> nextHybrid) {
+    public Node(int s, int e, int v, int t) {
         this.start = s;
         this.end = e;
         this.value = v;
         this.type = t;
-        this.next = next;
-        this.nextHybrid = nextHybrid;
+        this.next = new HashSet<>();
+        this.nextHybrid = new HashSet<>();
     }
+
+    @Override
+    public String toString() {
+        return "("+"Start:"+Integer.toString(start)+" end:"+Integer.toString(end)+" value:"+Integer.toString(value)+" type:" + Integer.toString(type) +")";
+    }
+
 }
